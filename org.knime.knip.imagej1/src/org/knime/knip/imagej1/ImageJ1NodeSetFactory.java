9499d08c4f8bd5ee04c59b48b673b5fc1336a116
Martin Horn <martin.horn@uni-konstanz.de>
2013-11-20 08:43:38 +0100

    after-id '/' replaced by empty after-id '' to get rid of error message in knime log

--- org.knime.knip.imagej1/src/org/knime/knip/imagej1/ImageJ1NodeSetFactory.java
+++ org.knime.knip.imagej1/src/org/knime/knip/imagej1/ImageJ1NodeSetFactory.java
@@ -60,7 +60,7 @@ import org.knime.knip.imagej1.io.IJImageReaderNodeFactory;
 import org.knime.knip.imagej1.plugin.reg.StackRegNodeFactory;
 
 /**
- * 
+ *
  * @author <a href="mailto:dietzc85@googlemail.com">Christian Dietz</a>
  * @author <a href="mailto:horn_martin@gmx.de">Martin Horn</a>
  * @author <a href="mailto:michael.zinsmaier@googlemail.com">Michael
@@ -113,7 +113,7 @@ public class ImageJ1NodeSetFactory implements NodeSetFactory {
 	 */
 	@Override
 	public String getAfterID(final String id) {
-		return "/";
+		return "";
 	}
 
 	/**
