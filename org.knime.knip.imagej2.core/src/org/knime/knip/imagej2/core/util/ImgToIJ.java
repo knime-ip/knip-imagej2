8d8b2e9d77e0e472d50df7190fec73ce610c1e9d
dietzc <christian.dietz@uni-konstanz.de>
2014-07-02 17:01:46 +0200

    ImgToIJ: Switch on Multi-Threading after test-flows were uploaded

--- org.knime.knip.imagej2.core/src/org/knime/knip/imagej2/core/util/ImgToIJ.java
+++ org.knime.knip.imagej2.core/src/org/knime/knip/imagej2/core/util/ImgToIJ.java
@@ -60,7 +60,6 @@ import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import net.imglib2.Cursor;
@@ -79,6 +78,9 @@ import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.view.Views;
 
+import org.knime.core.node.KNIMEConstants;
+import org.knime.knip.base.KNIPConstants;
+import org.knime.knip.base.ThreadPoolExecutorService;
 import org.knime.knip.core.ops.metadata.DimSwapper;
 
 /**
@@ -164,9 +166,9 @@ public final class ImgToIJ {
 
         // parallelization
         final ImageProcessor[] slices = new ImageProcessor[numSlices];
-        final ExecutorService service = Executors.newSingleThreadExecutor();
-        //                new ThreadPoolExecutorService(
-        //                        KNIMEConstants.GLOBAL_THREAD_POOL.createSubPool(KNIPConstants.THREADS_PER_NODE));
+        final ExecutorService service =
+                new ThreadPoolExecutorService(
+                        KNIMEConstants.GLOBAL_THREAD_POOL.createSubPool(KNIPConstants.THREADS_PER_NODE));
 
         final ArrayList<Future<Void>> futures = new ArrayList<Future<Void>>();
         final T inType = img.firstElement();
@@ -189,9 +191,7 @@ public final class ImgToIJ {
                 @Override
                 public Void call() throws Exception {
 
-                    final Cursor<T> cursor =
-                            Views.iterable(Views.interval(access, tmp))
-                                    .cursor();
+                    final Cursor<T> cursor = Views.iterable(Views.interval(access, tmp)).cursor();
 
                     final ImageProcessor ip = processorFactory.createProcessor(width, height, inType);
 
