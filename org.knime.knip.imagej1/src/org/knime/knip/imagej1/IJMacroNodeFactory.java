b191d7d3773b9f5990414b17aaf62f31d44d645e
Kevin Mader <kevinmader@gmail.com>
2015-03-30 14:03:37 +0200

    added histogram generation to snippets

--- org.knime.knip.imagej1/src/org/knime/knip/imagej1/IJMacroNodeFactory.java
+++ org.knime.knip.imagej1/src/org/knime/knip/imagej1/IJMacroNodeFactory.java
@@ -117,6 +117,8 @@ import org.knime.knip.imagej1.macro.SharpenIJMacro;
 import org.knime.knip.imagej1.macro.SkeletonizeIJMacro;
 import org.knime.knip.imagej1.macro.SubstractBackgroundIJMacro;
 import org.knime.knip.imagej1.macro.WatershedIJMacro;
+import org.knime.knip.imagej1.macro.CalculateHistogramIJMacro;
+
 import org.knime.knip.imagej1.prefs.IJ1Preferences;
 import org.knime.knip.imagej2.core.util.UntransformableIJTypeException;
 import org.knime.node2012.KnimeNodeDocument.KnimeNode;
@@ -390,6 +392,7 @@ public class IJMacroNodeFactory<T extends RealType<T>> extends
                 pool.put("Analyze Particles", AnalyzeParticlesIJMacro.class);
                 pool.put("Sharpen", SharpenIJMacro.class);
                 pool.put("Watersched", WatershedIJMacro.class);
+                pool.put("Histogram", CalculateHistogramIJMacro.class);
                 pool.put("Substract Background", SubstractBackgroundIJMacro.class);
                 pool.put("Enhance Local Constract (CLAHE)", CLAHEIJMacro.class);
                 pool.put("Exact Euclidean Distance Transform (3D)", EDT3DIJMacro.class);
