00cba49440c73d4b14b15eaaa5df54191eb8f245
dietzc <christian.dietz@knime.com>
2017-02-03 12:02:23 -0800

    add calculator service

--- org.knime.knip.imagej2.core/src/org/knime/knip/imagej2/core/IJGateway.java
+++ org.knime.knip.imagej2.core/src/org/knime/knip/imagej2/core/IJGateway.java
@@ -81,6 +81,7 @@ import org.scijava.util.ColorRGB;
 
 import net.imagej.DatasetService;
 import net.imagej.autoscale.AutoscaleService;
+import net.imagej.operator.CalculatorService;
 import net.imagej.units.UnitService;
 
 /**
@@ -113,8 +114,8 @@ public final class IJGateway {
      */
     @SuppressWarnings("unchecked")
     private static final Class<? extends Service>[] SUPPORTED_SERVICES =
-            new Class[]{UnitService.class, UIService.class, MenuService.class, ToolService.class, EventService.class, ObjectService.class,
-                    AutoscaleService.class, OptionsService.class, DatasetService.class};
+            new Class[]{UnitService.class, UIService.class, MenuService.class, ToolService.class, EventService.class,
+                    ObjectService.class, AutoscaleService.class, OptionsService.class, DatasetService.class, CalculatorService.class};
 
     // MEMBERS
 
